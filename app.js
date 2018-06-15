703c04162b16afc145c7d147fc5d3bc1c5eb55c0
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-06-15 12:57:48 -0400

    set up routes

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const layout = require("./views/layout");
-const { db } = require('./models');
+const models = require('./models');
+const wiki = require('./routes/wiki');
+const user = require('./routes/user');
 
-db.authenticate(). //I might need to pass an object with username / password, check docs 
+
+models.db.authenticate(). //I might need to pass an object with username / password, check docs 
 then(() => {
     console.log('connected to the database');
 })
@@ -13,15 +16,21 @@ then(() => {
 app.use(volleyball);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-
-
+//console.log(wiki);
+app.use('/wiki', wiki);
+//app.use('/user', user);
 
 app.get('/', (req, res, next) => {
     res.send(layout("Hello!"));
 })
 
-const PORT = 1337;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const init = async()=>{
+    await models.db.sync();
+    const PORT = 1337;
+    app.listen(PORT, () => {
+        console.log(`App listening in port ${PORT}`);
+    });
+}
+
+init();
--- models/index.js
+++ models/index.js
@@ -1,6 +1,51 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
-module.exports = {
-    db
-}
+// module.exports = {
+//     db
+// }
+
+const Page = db.define('pages',{
+    title: {
+        type: Sequelize.STRING,
+        validate: {
+            notNull: true
+        }
+    },
+    slug: {
+        type: Sequelize.STRING,
+        validate: {
+            notNull: true
+        }
+    },
+    content: {
+        type: Sequelize.TEXT,
+        validate: {
+            notNull: true
+        }
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed'),
+        validate: {
+            notNull: true
+        }
+    }
+});
+
+const User = db.define('user',{
+    name: {
+        type: Sequelize.STRING,
+        validate: {
+            notNull: true
+        }
+    },
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            notNull: true,
+            isEmail: true
+        }
+    }
+});
+
+module.exports = {db, Page, User};
